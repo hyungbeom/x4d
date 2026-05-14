23040270154ac456bfd179b40650170ed0696b03
USER <hblee@progist.co.kr>
2026-05-14 18:05:31 +0900

    123

--- src/app/brochure/gemma/page.tsx
+++ src/app/brochure/gemma/page.tsx
@@ -3,12 +3,11 @@ import Overlay1 from "@/components/bdtec/overlay/OverLay1";
 import ScrollIndicator from "@/utils/ScrollIndicator";
 import React from "react";
 import dynamic from 'next/dynamic';
+
 const Spline = dynamic(() => import('@splinetool/react-spline'), {
     ssr: false,
-    // 로딩 중일 때 보여줄 UI도 여기서 바로 설정할 수 있어요! (기존 Suspense 역할 대체)
-    loading: () => <div style={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>3D 로딩 중...</div>
+    loading: () => <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>3D 로딩 중...</div>
 });
-
 
 export default function Home() {
     return (
