0e0db7e8e75c358bb789b54ec8104929b5ddc74d
USER <hblee@progist.co.lkr>
2026-05-15 07:59:22 +0900

    fitst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-"use client";
+'use client'
 
-import HomeRobotLanding from "@/components/home/HomeRobotLanding";
+
+import dynamic from "next/dynamic";
+import React from "react";
+
+const Spline = dynamic(() => import('@splinetool/react-spline'), {
+    ssr: false,
+    loading: () => <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>3D 로딩 중...</div>
+});
+
 
 export default function Home() {
-    return <HomeRobotLanding />;
+    return (
+        <main>
+            <Spline
+                scene="https://prod.spline.design/UbexGr1CFJhPJV7l/scene.splinecode"
+            />
+        </main>
+    );
 }
