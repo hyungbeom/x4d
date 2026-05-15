a2c1b450c5f9fa8f1de3ddfe44206d2b4908ef3c
USER <hblee@progist.co.lkr>
2026-05-15 12:14:06 +0900

    fitst

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-
 import dynamic from "next/dynamic";
 import React from "react";
 
@@ -9,12 +8,13 @@ const Spline = dynamic(() => import('@splinetool/react-spline'), {
     loading: () => <div style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>3D 로딩 중...</div>
 });
 
-
 export default function Home() {
     return (
-        <main>
+        // 🚀 여기에 스타일을 추가해서 화면을 100% 꽉 채워줍니다!
+        <main style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
             <Spline
                 scene="https://prod.spline.design/UbexGr1CFJhPJV7l/scene.splinecode"
+                style={{ width: '100%', height: '100%' }}
             />
         </main>
     );
